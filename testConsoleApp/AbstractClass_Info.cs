12f961c93da56d7f32ba2f75429771ee2b434e82
durga prakash <dprakash03@gmail.com>
2023-05-04 10:39:24 +0530

    #04-05-2023_1

---
+++ testConsoleApp/AbstractClass_Info.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testConsoleApp
+{
+    public abstract class AbstractClass_Info
+    {
+        
+        static AbstractClass_Info()
+        {
+        }
+        public AbstractClass_Info()
+            {}
+    }
+
+    interface IClass_Info
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- testConsoleApp/Program.cs
+++ testConsoleApp/Program.cs
@@ -61,6 +61,8 @@ namespace testConsoleApp
             }
             return thirdlarge;
         }
+
+
     }
 }
 
